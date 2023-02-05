45e9ecdd2db3f188dda83704f4e999e8c9b15560
william <william.jsson@hotmail.com>
2023-02-05 01:16:37 +0100

    Search Polished

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import pymongo
+import re
 from misc import personMaster
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -34,35 +35,135 @@ def generate_people():
     time.sleep(2)
 
 
+def fuzzy_matching():
+    res = mycol.find({"$text": {"$search": "Thoma"}})
+    print(list(res))
+
+fuzzy_matching()
+
+
 def search():
     while True:
         clear()
         tab_down()
         print(f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
-              f"-     Identity Search     -\n"
+              f"-  |   Identity Search  | -\n"
+              f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
+              f"-    [1]- First Name      -\n"
+              f"+    [2]- Last Name       +\n"
+              f"-    [3]- P'IdentNum      -\n"
+              f"+    [4]- Eye Colour      +\n"
+              f"-    [5]- Height          -\n"
+              f"+    [6]- Nationality     +\n"
+              f"-    [7]- Age             -\n"
               f"+                         +\n"
               f"-      [ E/e(Exit) ]      -\n"
               f"+-+-+-+-+-+-+-+-+-+-+-+-+-+")
         tab_down()
-        usr_sel = input(" Name >> ")
-        usr_sel = usr_sel.strip(" ").capitalize()
+        usr_sel = input(" >> ")
         if usr_sel.lower().strip(" ") == "e":
             break
-        else:
-            name_query = {"first_name": f"{usr_sel}"}
-            curs = mycol.find(name_query)
-            tab_down()
-            line()
-            for i in curs:
-                print(
-                    f"{i.get('first_name')} {i.get('last_name')}, {i.get('age')} y/o, {i.get('height')}cm, {i.get('nationality')}, ({i.get('personal_identification_number')})")
-                line()
-            del curs
-            enter_to_continue()
+        elif usr_sel.lower().strip(" ") == "1":
+            # First_name Search
+            usr_sel = input(" Name >> ")
+            usr_sel = usr_sel.strip(" ").capitalize()
+            if usr_sel.lower().strip(" ") == "e":
+                break
+            else:
+                filter = ({
+                    'first_name': re.compile(r".*" + f"{usr_sel}" + r".*")
+                })
 
+                dbCurs = mycol.find(
+                    filter=filter
+                )
+
+        elif usr_sel.lower().strip(" ") == "2":
+            # Last_name search
+            usr_sel = input(" Surname >> ")
+            usr_sel = usr_sel.strip(" ").capitalize()
+            if usr_sel.lower().strip(" ") == "e":
+                break
+            else:
+                filter = ({
+                    'last_name': re.compile(r".*" + f"{usr_sel}" + r".*")
+                })
+
+                dbCurs = mycol.find(
+                    filter=filter
+                )
+
+        elif usr_sel.lower().strip(" ") == "3":
+            # Personal Identification Number search
+            usr_sel = input(" Personal Identification Number (ex. 19990101-9984) >> ")
+            usr_sel = usr_sel.strip(" ").capitalize()
+            if usr_sel.lower().strip(" ") == "e":
+                break
+            else:
+                filter = ({
+                    'personal_identification_number': re.compile(r".*" + f"{usr_sel}" + r".*")
+                })
+
+                dbCurs = mycol.find(
+                    filter=filter
+                )
+
+        elif usr_sel.lower().strip(" ") == "4":
+            # Eye Colour Search
+            usr_sel = input(" Eye Colour >> ")
+            usr_sel = usr_sel.strip(" ").capitalize()
+            if usr_sel.lower().strip(" ") == "e":
+                break
+            else:
+                filter = ({
+                    'eye_colour': re.compile(r".*" + f"{usr_sel}" + r".*")
+                })
+
+                dbCurs = mycol.find(
+                    filter=filter
+                )
+
+        elif usr_sel.lower().strip(" ") == "5":
+            # Height Search
+            print("[Currently under maintenance, No results will be returned. Please check back later or contact the "
+                  "administration for further "
+                  "instructions...]")
+            usr_sel = input(" Height(cm) >> ")
+            usr_sel = usr_sel.strip(" ").capitalize()
+            if usr_sel.lower().strip(" ") == "e":
+                break
+            else:
+                dbCurs = mycol.find({"height": f"{int(usr_sel)}"})
+
+        elif usr_sel.lower().strip(" ") == "6":
+            # Nationality search
+            usr_sel = input(" Nationality >> ")
+            usr_sel = usr_sel.strip(" ").capitalize()
+            if usr_sel.lower().strip(" ") == "e":
+                break
+            else:
+                filter = ({
+                    'nationality': re.compile(r".*" + f"{usr_sel}" + r".*")
+                })
+
+                dbCurs = mycol.find(
+                    filter=filter
+                )
+
+        else:
+            pass
+
+        tab_down()
+        line()
+        for i in dbCurs:
+            print(
+                f"{i.get('first_name')} {i.get('last_name')}, {i.get('age')} y/o, {i.get('height')}cm, {i.get('nationality')}, ({i.get('personal_identification_number')})")
+            line()
+        enter_to_continue()
+        
 
 def line():
-    print("===============================================================")
+    print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
 
 
 def clear():
@@ -89,6 +190,7 @@ def get_statistics(selection: int):
 def main():
     clear()
     while True:
+        clear()
         print(f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
               f"-  G/g(Generate People)   -\n"
               f"+  L/l(List Register)     +\n"
