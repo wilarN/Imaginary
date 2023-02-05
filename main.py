84aa261759cee9b289762e31945f3a3da93981a8
william <william.jsson@hotmail.com>
2023-02-05 01:38:50 +0100

    Minor Changes

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import time
 import pymongo
 import re
 from misc import personMaster
+import pystyle as ps
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["imaginaryMaster"]
 mycol = mydb["people"]
+global dbCurs
 dbCurs = mycol.find({})
 
 
@@ -15,6 +17,51 @@ def enter_to_continue():
     print("", flush=True)
     input("Press enter to continue...")
 
+def styled_coloured_print_centered(text, colour=None, instant=False):
+    if colour is not None:
+        col = colour
+    else:
+        col = None
+    if instant:
+        time_delay = 0
+    else:
+        time_delay = 0.01
+    '''
+    Default colour "None" --> cyan_to_green
+    red --> red
+    blue --> cyan
+    orange --> orange
+    pink --> pink
+    yellow --> yellow
+    bluegreen --> bluegreen
+    purpleblue --> purple to blue gradient
+    greenyellow --> greenyellow
+    Default instant --> False
+    '''
+    if colour is None:
+        ps.Write.Print(text=ps.Center.XCenter(text), color=ps.Colors.cyan, interval=time_delay)
+    else:
+        if colour == "red":
+            col = ps.Colors.red
+        elif colour == "orange":
+            col = ps.Colors.orange
+        elif colour == "blue":
+            col = ps.Colors.cyan
+        elif colour == "pink":
+            col = ps.Colors.pink
+        elif colour == "greenwhite":
+            col = ps.Colors.green_to_white
+        elif colour == "purpleblue":
+            col = ps.Colors.purple_to_blue
+        elif colour == "yellow":
+            col = ps.Colors.yellow
+        elif colour == "bluegreen":
+            col = ps.Colors.blue_to_green
+        elif colour == "greenyellow":
+            col = ps.Colors.green_to_yellow
+
+        ps.Write.Print(text=ps.Center.XCenter(text), color=col, interval=time_delay)
+    print("", flush=True)
 
 def generate_people():
     while True:
@@ -43,10 +90,27 @@ fuzzy_matching()
 
 
 def search():
+    global dbCurs
+    clear()
+    styled_coloured_print_centered(text="""
+    ##############################################################################
+    # We trust you have received the usual lecture from the                      #
+    # Administrator or Organisation. It usually boils down to these four things: #
+    #                                                                            #
+    # #1) Respect the privacy of others.                                         #
+    # #2) Think before you type/search.                                          #
+    # #3) Reason for your lookup.                                                #
+    # #4) With great power comes great responsibility.                           #
+    #                                                                            #
+    ##############################################################################
+    """, instant=True, colour="yellow")
+    enter_to_continue()
+
+
     while True:
         clear()
         tab_down()
-        print(f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
+        styled_coloured_print_centered(text=f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
               f"-  |   Identity Search  | -\n"
               f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
               f"-    [1]- First Name      -\n"
@@ -55,10 +119,10 @@ def search():
               f"+    [4]- Eye Colour      +\n"
               f"-    [5]- Height          -\n"
               f"+    [6]- Nationality     +\n"
-              f"-    [7]- Age             -\n"
+              f"-                         -\n"
               f"+                         +\n"
               f"-      [ E/e(Exit) ]      -\n"
-              f"+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+              f"+-+-+-+-+-+-+-+-+-+-+-+-+-+", instant=True, colour="blue")
         tab_down()
         usr_sel = input(" >> ")
         if usr_sel.lower().strip(" ") == "e":
@@ -157,10 +221,10 @@ def search():
         line()
         for i in dbCurs:
             print(
-                f"{i.get('first_name')} {i.get('last_name')}, {i.get('age')} y/o, {i.get('height')}cm, {i.get('nationality')}, ({i.get('personal_identification_number')})")
+                f"{i.get('first_name')} {i.get('last_name')}, {i.get('sex')}, {i.get('age')} y/o, {i.get('height')}cm, {i.get('nationality')}, ({i.get('personal_identification_number')})")
             line()
         enter_to_continue()
-        
+
 
 def line():
     print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
@@ -188,19 +252,21 @@ def get_statistics(selection: int):
 
 
 def main():
+    global dbCurs
     clear()
     while True:
         clear()
-        print(f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
-              f"-  G/g(Generate People)   -\n"
-              f"+  L/l(List Register)     +\n"
-              f"-  S/s(Search)            -\n"
-              f"+                         +\n"
-              f"-                         -\n"
-              f"+      [ E/e(Exit) ]      +\n"
-              f"-                         -\n"
-              f"+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
-              f"In register:{get_statistics(1)}")
+        styled_coloured_print_centered(text=
+              f"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
+              f"-       G/g(Generate People) - (Admin)      -\n"
+              f"+       L/l(List Register) - (DB_Heavy)     +\n"
+              f"-       S/s(Search) - (General)             -\n"
+              f"+                                           +\n"
+              f"-                                           -\n"
+              f"+          [ E/e(Exit) ]                    +\n"
+              f"-                                           -\n"
+              f"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n"
+              f"In register:{get_statistics(1)}", instant=True, colour="orange")
         tab_down()
         usr_sel = input(" >> ")
         usr_sel = usr_sel.lower().strip(" ")
@@ -218,6 +284,7 @@ def main():
         elif usr_sel.__contains__("e"):
             # Exit
             print("Exiting...")
+            del dbCurs
             exit(0)
 
 
