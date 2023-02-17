51e2bc334ccbc95e1aa6a8a8fba84d46c69ca408
William <william.jsson@hotmail.com>
2023-02-18 00:57:03 +0100

    Minor Fixes and Additions

--- src/logging.py
+++ src/logging.py
@@ -1,5 +1,5 @@
 import datetime
-from src.headers import *
+import src.headers as head
 import src.globals as glob
 
 currencies = ['United States Dollar (USD)', 'Euro (EUR)', 'Japanese Yen (JPY)', 'British Pound (GBP)', 'Swiss Franc (CHF)', 'Canadian Dollar (CAD)', 'Australian Dollar (AUD)', 'New Zealand Dollar (NZD)', 'Chinese Yuan Renminbi (CNY)', 'Hong Kong Dollar (HKD)', 'Singapore Dollar (SGD)', 'South Korean Won (KRW)', 'Indian Rupee (INR)', 'Brazilian Real (BRL)', 'Russian Ruble (RUB)', 'South African Rand (ZAR)']
@@ -7,13 +7,13 @@ currencies = ['United States Dollar (USD)', 'Euro (EUR)', 'Japanese Yen (JPY)', 
 
 def log_transaction(sender, reciever, amount, successful: bool = True):
     if successful:
-        dyn_text = f"[TRANSACTION SUCCESSFUL] [{sender['account_ownership']}]({sender['account_identification']}) >- [{amount} {src.headers.random.choice(currencies)}] -> [{reciever['account_ownership']}]({reciever['account_identification']})"
-        src.headers.styled_coloured_print_centered(
+        dyn_text = f"[TRANSACTION SUCCESSFUL] [{sender['account_ownership']}]({sender['account_identification']}) >- [{amount} {head.random.choice(currencies)}] -> [{reciever['account_ownership']}]({reciever['account_identification']})"
+        head.styled_coloured_print_centered(
             text=dyn_text,
             instant=True, colour="green")
     else:
-        dyn_text = f"[TRANSACTION FAILED] {sender['account_ownership']}'s account ({sender['account_identification']}) has insufficient balance to transfer {amount} {src.headers.random.choice(currencies)}."
-        src.headers.styled_coloured_print_centered(
+        dyn_text = f"[TRANSACTION FAILED] {sender['account_ownership']}'s account ({sender['account_identification']}) has insufficient balance to transfer {amount} {head.random.choice(currencies)}."
+        head.styled_coloured_print_centered(
             text=dyn_text,
             instant=True, colour="orange")
-    src.headers.write_to_file(text_to_write=f"[{datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}] {dyn_text}\n", typeOfWrite="a", path_to_file=glob.settings_file_location)
+    head.write_to_file(text_to_write=f"[{datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}] {dyn_text}\n", typeOfWrite="a", path_to_file=glob.settings_file_location)
--- src/virt_messages.py
+++ src/virt_messages.py
@@ -1,13 +1,64 @@
 import markovify
+from time import sleep
 
-# Load the input text file
-with open("./data/text_data/WizardsOfEldoria.txt", 'r', errors='ignore') as f:
-    text = f.read()
 
-# Build the Markov chain model
-text_model = markovify.Text(text)
+def pre_init():
+    # Load the input text file for the "model" or whatever.
+    file = open("src/data/text_data/WizardsOfEldoria.txt", 'r', errors='ignore')
+    text = file.read()
+    file.close()
+    return text
 
-# Generate 5 random sentences
-for i in range(5):
-    sentence = text_model.make_sentence()
-    print(sentence)
+
+def simulate_comms():
+    text = pre_init()
+    from src.simulation import stop_event, head, glob, choice, log_transaction
+    # Build the Markov chain model
+    text_model = markovify.Text(text)
+
+    while not stop_event.is_set():
+        sleep(head.random.uniform(0, 1))
+
+
+        # Get two random customers from the database
+        customers = glob.mycol.aggregate([{"$sample": {"size": 2}}])
+
+        # Perform transaction between the two customers
+        customer_list = list(customers)
+        sender = customer_list[0]
+        receiver = customer_list[1]
+
+        sender_account = choice(sender["bank_accounts"])
+        # amount = 100  # Change this to the desired transaction amount
+        amount = head.random.randint(100, 100000)
+
+        receiver_account = choice(receiver["bank_accounts"])
+
+        if sender_account["balance"] >= amount:
+            glob.mycol.update_one({"_id": sender["_id"]},
+                                  {"$inc": {"bank_accounts.$[elem].balance": -amount}},
+                                  array_filters=[
+                                      {"elem.account_identification": sender_account["account_identification"]}])
+            glob.mycol.update_one({"_id": receiver["_id"]},
+                                  {"$inc": {"bank_accounts.$[elem].balance": amount}}, array_filters=[
+                    {"elem.account_identification": receiver_account["account_identification"]}])
+            # head.styled_coloured_print_centered(text=f"\n[TRANSACTION SUCCESSFUL] [[{sender_account['account_ownership']}]({sender_account['account_identification']}) >- [{amount} {head.random.choice(currencies)}] -> [{receiver_account['account_ownership']}]({receiver_account['account_identification']})]", instant=True, colour="green")
+            log_transaction(successful=True, sender=sender_account, reciever=receiver_account, amount=amount)
+        else:
+            # head.styled_coloured_print_centered(text=f"\n[TRANSACTION FAILED] {sender_account['account_ownership']}'s account ({sender_account['account_identification']}) has insufficient balance to transfer {amount}", instant=True, colour="orange")
+            log_transaction(successful=False, sender=sender_account, reciever=receiver_account, amount=amount)
+
+
+
+
+
+
+
+
+
+
+
+        # Generate 5 random sentences
+        for i in range(5):
+            sentence = text_model.make_sentence()
+            print(sentence)
