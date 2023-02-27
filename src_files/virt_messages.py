0482bd344762cfaa148746645fa36d9c8792bd24
William <william.jsson@hotmail.com>
2023-02-27 19:19:44 +0100

    Add reddit data comments for markovify

--- src_files/virt_messages.py
+++ src_files/virt_messages.py
@@ -1,5 +1,19 @@
 import markovify
 from time import sleep
+import json
+import os
+
+# Open the JSON file and extract the "body" field from each comment
+messages = []
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(f'{dir_path}/data/text_data/RC_2008-09') as f:
+    for line in f:
+        comment = json.loads(line)
+        if 'body' in comment:
+            messages.append(comment['body'])
+
+# Print the first 10 messages
+print(messages[:10])
 
 
 def pre_init():
@@ -11,54 +25,16 @@ def pre_init():
 
 
 def simulate_comms():
-    text = pre_init()
-    from simulation import stop_event, head, glob, choice, log_transaction
+    # text = pre_init()
+    text = messages
+    from src_files.simulation import stop_event, head, glob, random, log_transaction
     # Build the Markov chain model
     text_model = markovify.Text(text)
 
     while not stop_event.is_set():
         sleep(head.random.uniform(0, 1))
 
-
-        # Get two random customers from the database
-        customers = glob.mycol.aggregate([{"$sample": {"size": 2}}])
-
-        # Perform transaction between the two customers
-        customer_list = list(customers)
-        sender = customer_list[0]
-        receiver = customer_list[1]
-
-        sender_account = choice(sender["bank_accounts"])
-        # amount = 100  # Change this to the desired transaction amount
-        amount = head.random.randint(100, 100000)
-
-        receiver_account = choice(receiver["bank_accounts"])
-
-        if sender_account["balance"] >= amount:
-            glob.mycol.update_one({"_id": sender["_id"]},
-                                  {"$inc": {"bank_accounts.$[elem].balance": -amount}},
-                                  array_filters=[
-                                      {"elem.account_identification": sender_account["account_identification"]}])
-            glob.mycol.update_one({"_id": receiver["_id"]},
-                                  {"$inc": {"bank_accounts.$[elem].balance": amount}}, array_filters=[
-                    {"elem.account_identification": receiver_account["account_identification"]}])
-            # head.styled_coloured_print_centered(text=f"\n[TRANSACTION SUCCESSFUL] [[{sender_account['account_ownership']}]({sender_account['account_identification']}) >- [{amount} {head.random.choice(currencies)}] -> [{receiver_account['account_ownership']}]({receiver_account['account_identification']})]", instant=True, colour="green")
-            log_transaction(successful=True, sender=sender_account, reciever=receiver_account, amount=amount)
-        else:
-            # head.styled_coloured_print_centered(text=f"\n[TRANSACTION FAILED] {sender_account['account_ownership']}'s account ({sender_account['account_identification']}) has insufficient balance to transfer {amount}", instant=True, colour="orange")
-            log_transaction(successful=False, sender=sender_account, reciever=receiver_account, amount=amount)
-
-
-
-
-
-
-
-
-
-
-
         # Generate 5 random sentences
         for i in range(5):
-            sentence = text_model.make_sentence()
+            sentence = text_model.make_short_sentence(min_chars=20, max_chars=30)
             print(sentence)
